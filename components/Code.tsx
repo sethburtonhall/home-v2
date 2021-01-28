82e7830f3b8710d0d228550ed565a0c6fa0b51ff
Seth Hall <sethburtonhall@gmail.com>
2021-01-28 16:59:52 -0500

    fixes

--- components/Code.tsx
+++ components/Code.tsx
@@ -32,9 +32,9 @@ export default function Code() {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M4 6h16M4 12h16m-7 6h7"
                 />
               </svg>
@@ -48,9 +48,9 @@ export default function Code() {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
