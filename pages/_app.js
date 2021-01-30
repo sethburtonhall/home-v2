97b1e7ce6280f696762c94cff247c49ee91b22b6
Seth Hall <sethburtonhall@gmail.com>
2021-01-29 22:13:23 -0500

    clean up

--- pages/_app.js
+++ pages/_app.js
@@ -3,29 +3,6 @@ import { ModeContext } from '../contexts/ModeContext'
 import { SideBarContext } from '../contexts/SideBarContext'
 import '../styles/globals.css'
 
-export function reportWebVitals(metric) {
-  switch (metric.name) {
-    case 'FCP':
-      // handle FCP results
-      break
-    case 'LCP':
-      // handle LCP results
-      break
-    case 'CLS':
-      // handle CLS results
-      break
-    case 'FID':
-      // handle FID results
-      break
-    case 'TTFB':
-      // handle TTFB results
-      break
-    default:
-      break
-  }
-    console.log(metric)
-}
-
 export default function MyApp({ Component, pageProps }) {
   const [mode, setMode] = useState()
   const [toggle, setToggle] = useState()
--- pages/portfolio.tsx
+++
@@ -1,5 +0,0 @@
-export default function Portfolio() {
-  return (
-    <header className="text-5xl">This is my portfolio</header>
-  )
-}
