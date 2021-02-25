938d54d2f38325b7c1a75f345b085128d4414b28
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2021-02-25 10:01:01 -0500

    adds social card

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,36 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Dosis:wght@700&family=Roboto+Slab:wght@300;400&display=swap"
             rel="stylesheet"
           />
+          {/* Open Graph */}
+          <meta
+            property="og:url"
+            content="https://www.sethhallcreative.com/"
+            key="ogurl"
+          />
+          <meta property="og:image" content="/social-card.png" key="ogimage" />
+          <meta
+            property="og:site_name"
+            content="Seth Hall Creative - Code:Film"
+            key="ogsitename"
+          />
+          <meta
+            property="og:title"
+            content="Seth Hall Creative - Code:Film"
+            key="ogtitle"
+          />
+          +{' '}
+          <meta
+            property="og:description"
+            content="Frontend/Full Stack Web Developer. Creative Producer."
+            key="ogdesc"
+          />
+          {/* Twitter */}
+          <meta name="twitter:card" content="summary" key="twcard" />
+          <meta
+            name="twitter:creator"
+            content="@sethburtonhall"
+            key="twhandle"
+          />
         </Head>
         <body
           className={`bg-purple-900 text-white ${
